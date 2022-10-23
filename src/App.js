9cb1567c9443d718e463f7749d4c9176f259d77b
Bruno Miguel <113399299+BMiguelDev@users.noreply.github.com>
2022-10-23 05:53:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,293 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+import './App.scss';
+import Question from './components/Question/Question';
+
+// Local storage constants
+const LOCAL_STORAGE_KEY_IS_QUIZ_STARTED = "QuizzicalApp.isQuizStarted";
+const LOCAL_STORAGE_KEY_QUIZZ_QUESTIONS = "QuizzicalApp.quizzQuestions";
+const LOCAL_STORAGE_KEY_QUIZZ_RESULTS = "QuizzicalApp.quizzResults";
+const LOCAL_STORAGE_KEY_IS_LOADING = "QuizzicalApp.isLoading";
+const LOCAL_STORAGE_KEY_IS_DARK_MODE = "QuizzicalApp.isDarkMode";
+const LOCAL_STORAGE_KEY_QUIZZ_CATEGORIES_ARRAY = "QuizzicalApp.quizzCategoriesArray";
+const LOCAL_STORAGE_KEY_QUIZZ_OPTIONS = "QuizzicalApp.quizzOptions";
+
+
+export default function App() {
+
+  //State variables
+
+  // Boolean variable that indicates whether the quizz has been started or not
+  const [isQuizzStarted, setIsQuizzStarted] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IS_QUIZ_STARTED)) || false);
+
+  // Variable that holds the array of 5 questions of the current quizz
+  const [quizzQuestions, setQuizzQuestions] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_QUIZZ_QUESTIONS)) || []);
+
+  // Variable for quizz results, Array[<isResultsShown>, <counterCorrectAnswers>]
+  const [quizzResults, setQuizzResults] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_QUIZZ_RESULTS)) || [false, 0]);
+
+  // Boolean variable that indicates if data is being loaded to the app
+  const [isLoading, setIsLoading] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IS_LOADING)) || false);
+
+  // Boolean variable that indicates if dark mode is on or off
+  // Some functionality replaced with UseRef to prevent unnecessary re-renders
+  const [isDarkMode, setIsDarkMode] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IS_DARK_MODE)) || false);
+
+  // Variable that holds the array of possible question categories to choose from
+  const [quizzCategoriesArray, setQuizzCategoriesArray] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_QUIZZ_CATEGORIES_ARRAY)) || []);
+
+  // Variable object that stores the initial options choose for the current quizz
+  const [quizzOptions, setQuizzOptions] = useState(() =>
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_QUIZZ_OPTIONS)) || { category: "", difficulty: "", urlUsed: "" });
+
+  // Variable to handle dark mode without unnecessary re-renders
+  let darkModeRef = useRef();
+
+  // On first component mount, fetch question categories from API and store then in the <quizzCategoriesArray> variable
+  useEffect(() => {
+    if (quizzCategoriesArray.length === 0) {
+      fetch("https://opentdb.com/api_category.php")
+        .then(res => res.json())
+        .then(data => setQuizzCategoriesArray(data.trivia_categories));
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_IS_QUIZ_STARTED, JSON.stringify(isQuizzStarted));
+  }, [isQuizzStarted])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_QUIZZ_QUESTIONS, JSON.stringify(quizzQuestions));
+  }, [quizzQuestions])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_QUIZZ_RESULTS, JSON.stringify(quizzResults));
+  }, [quizzResults])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_IS_LOADING, JSON.stringify(isLoading));
+  }, [isLoading])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_IS_DARK_MODE, JSON.stringify(isDarkMode));
+  }, [isDarkMode])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_QUIZZ_CATEGORIES_ARRAY, JSON.stringify(quizzCategoriesArray));
+  }, [quizzCategoriesArray])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_QUIZZ_OPTIONS, JSON.stringify(quizzOptions));
+  }, [quizzOptions])
+
+
+  // Function that fetches questions from API based on the options chosen by the user, and stores them in the <quizzQuestions> variable
+  function handleQuizzStart(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    if (quizzOptions.category === "") {
+      if (quizzOptions.difficulty === "") {
+        const url = "https://opentdb.com/api.php?amount=5";
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            // Store the url used to be able to quickly fetch the same url upon quizz restart
+            setQuizzOptions(prevQuizzOptions => ({ ...prevQuizzOptions, urlUsed: url }));
+            setQuizzQuestions(data.results);
+            setIsLoading(false);
+          });
+      } else {
+        const url = "https://opentdb.com/api.php?amount=5&difficulty=" + quizzOptions.difficulty;
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            setQuizzOptions(prevQuizzOptions => ({ ...prevQuizzOptions, urlUsed: url }));
+            setQuizzQuestions(data.results);
+            setIsLoading(false);
+          });
+      }
+    } else if (quizzOptions.difficulty === "") {
+      const url = "https://opentdb.com/api.php?amount=5&category=" + quizzOptions.category;
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setQuizzOptions(prevQuizzOptions => ({ ...prevQuizzOptions, urlUsed: url }));
+          setQuizzQuestions(data.results);
+          setIsLoading(false);
+        });
+    } else {
+      const url = "https://opentdb.com/api.php?amount=5&category=" + quizzOptions.category + "&difficulty=" + quizzOptions.difficulty;
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setQuizzOptions(prevQuizzOptions => ({ ...prevQuizzOptions, urlUsed: url }));
+          setQuizzQuestions(data.results);
+          setIsLoading(false);
+        });
+    }
+
+    setIsQuizzStarted(true);
+  }
+
+  // Function that updates a question's selected answer
+  function handleSelectAnswer(index, answer) {
+    if (quizzResults[0] === true) return;   // Don't do anything if results are already being shown
+    setQuizzQuestions(prevQuizzQuestions => {
+      let newQuizzQuestions = prevQuizzQuestions.slice();
+      newQuizzQuestions[index] = { ...newQuizzQuestions[index], answerSelected: answer }
+      return newQuizzQuestions;
+    })
+  }
+
+  // Function that toggles between showing results and restarting quizz using same options
+  function handleCheckResults() {
+    if (quizzResults[0] === true) {
+      setIsLoading(true);
+      setQuizzResults([false, 0]);
+      // Fetch previous url to get new set of 5 questions using the same options
+      fetch(quizzOptions.urlUsed)
+        .then((res) => res.json())
+        .then((data) => {
+          setQuizzQuestions(data.results);
+          setIsLoading(false);
+        });
+
+    }
+    else {
+      let counter = 0;
+      // Count number of correct answers
+      quizzQuestions.forEach(question => {
+        if (question.answerSelected === question.correct_answer) counter++;
+      })
+      setQuizzResults([true, counter]);
+    }
+  }
+
+  // Function that toggles the <isDarkMode> variable and updates <darkModeRef> ref variable to add/remove the "dark-mode" className
+  function handleDarkMode() {
+    setIsDarkMode(prevIsDarkMode => !prevIsDarkMode)
+    darkModeRef.current.className = (darkModeRef.current.className === "app-container") ? "app-container dark-mode" : "app-container";
+  }
+
+  // Function that updates the <quizzOptions> variable object according the the options chosen by the user
+  function handleQuizzOptionChange(event) {
+    const { name, value } = event.target;
+    setQuizzOptions(prevQuizzOptions => ({ ...prevQuizzOptions, [name]: value }));
+  }
+
+  // Function that resets quizz
+  function handleQuizzReset() {
+    setIsQuizzStarted(false);
+    setQuizzResults([false, 0]);
+    setQuizzQuestions([]);
+  }
+
+  // Constant holding the completion bar filling styles, which varry based on the number of corret answers
+  const borderFillingStyles = {
+    width: quizzResults[1] > 0 ? quizzResults[1] * 20 + "%" : "10%",
+    padding: quizzResults[1] > 0 ? ".7rem" : "0.7rem .2rem",
+    border: quizzResults[1] === 0 && "none",
+    left: quizzResults[1] === 0 && "0%",
+  }
+
+  return (
+    <div ref={darkModeRef} className="app-container" /*className="app-container"*//*className={isDarkMode ? "app-container dark-mode" : "app-container"}*/>
+      {
+        isQuizzStarted ?
+          (
+            <div className="quizz-container">
+              {
+                isLoading ?
+                  <div className="loading-container">
+                    <i className="fas fa-spinner fa-spin"></i>
+                    {/* <i className="fa-solid fa-book fa-spin"></i> */}
+                  </div> :
+                  <div className="questions-container">
+                    {quizzQuestions.map((questionData, index) =>
+                      <Question
+                        key={questionData.question}
+                        questionData={questionData}
+                        questionIndex={index}
+                        handleSelectAnswer={handleSelectAnswer}
+                        isShowResults={quizzResults[0]} />)}
+                  </div>
+              }
+              <div className="results-container">
+                <div className={quizzResults[0] === false ? "results-description results-description-hide" : "results-description"}>
+                  <h3 className="results-correct-answers">You scored {quizzResults[1]}/5 correct answers</h3>
+                  <span className="completion-bar-frame"><span style={borderFillingStyles} className="completion-bar-filling"></span></span>
+                </div>
+                <div className="results-buttons-container">
+                  <button className="quizz-action-button" onClick={handleCheckResults}>{quizzResults[0] === true ? "Play Again" : "Check Answers"}</button>
+                  <button className="quizz-reset-button" onClick={handleQuizzReset}>Reset Quizz</button>
+                </div>
+              </div>
+            </div>
+          ) :
+          (
+            <div className="unstarted-quizz-container">
+              <h2 className="unstarted-quizz-title">Quizz Challenge</h2>
+              <p className="unstarted-quizz-text">
+                Choose random trivia questions and <br />
+                answer as many as you can!
+              </p>
+              <form onSubmit={handleQuizzStart}>
+                <div className="unstarted-quizz-inputs-container">
+                  <label>
+                    Category
+                    <br />
+                    <select onChange={handleQuizzOptionChange} name="category" value={quizzOptions.category}>
+                      <option value="">Any</option>
+                      {quizzCategoriesArray.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
+                    </select>
+                  </label>
+                  <label>
+                    Difficulty
+                    <br />
+                    <select onChange={handleQuizzOptionChange} name="difficulty" value={quizzOptions.difficulty}>
+                      <option value="">Any</option>
+                      <option value="easy">Easy</option>
+                      <option value="medium">Medium</option>
+                      <option value="hard">Hard</option>
+                    </select>
+                  </label>
+                </div>
+                <button type="submit" className="unstarted-quizz-button" /*onClick={handleQuizzStart}*/>Start Quizz</button>
+              </form>
+            </div >
+          )
+      }
+      <footer className="footer_container">
+        <div className="footer_api_description">
+          <p>Powered by <a href="https://opentdb.com/" target="_blank" rel="noreferrer">Open Trivia Database API</a></p>
+        </div>
+        <div className="footer_text_container">
+          <div className="footer_text">
+            <p>Copyright © 2022 Bruno Miguel</p>
+          </div>
+          <div className="footer_icon_container">
+            <a href="https://google.com" target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://google.com" target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+      <button className="dark-mode-button" onClick={handleDarkMode}>
+        {
+          isDarkMode ?
+            <i className="fa-solid fa-moon"></i> :
+            <i className="fa-solid fa-sun"></i>
+        }
+      </button>
+    </div >
+  )
+}
---
+++ src/components/Question/Question.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { nanoid } from "nanoid"
+
+import styles from './Question.module.scss';
+
+
+export default function Question(props) {
+
+  // Variable that holds an array with the answers scrambled (random order)
+  const [scrambledAnswers, setScrambledAnswers] = useState([]);
+
+  // Function that decodes strange characters in the question string
+  function decodeQuestionString(string) {
+    var text = document.createElement("textarea");
+    text.innerHTML = string;
+    return text.value;
+  }
+
+  // On first component mount, create array with all answers in a scrambled order and store it in <scrambledAnswers> variable
+  useEffect(() => {
+    if (scrambledAnswers.length === 0) {
+      const newAnswersArray = props.questionData.incorrect_answers.slice();
+      newAnswersArray.push(props.questionData.correct_answer);
+      setScrambledAnswers(newAnswersArray.sort((a, b) => 0.5 - Math.random()));
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+  // Function that decides className property of answer for correct coloring when quizz ends
+  function getAnswerClassName(answer) {
+    const isShowResults = props.isShowResults;
+    if (isShowResults) {
+      return answer === props.questionData.correct_answer ?
+        (answer === props.questionData.answerSelected ? `${styles.answer} ${styles.answer_selected} ${styles.answer_correct}` : `${styles.answer} ${styles.answer_correct}`) :
+        (answer === props.questionData.answerSelected ? `${styles.answer} ${styles.answer_incorrect} ${styles.answer_selected}` : `${styles.answer} ${styles.answer_dimmed}`)
+
+    } else {
+      return props.questionData.answerSelected === answer ? `${styles.answer} ${styles.answer_selected}` : styles.answer
+    }
+  }
+
+
+  return (
+    <div className={styles.question_container}>
+      <h3 className={styles.question}>{decodeQuestionString(props.questionData.question)}</h3>
+      <div className={styles.answers_container}>
+        {
+          scrambledAnswers.map(answer => (
+            <div key={nanoid()} className={styles.answer_container}>
+              <p
+                className={getAnswerClassName(answer)}
+                onClick={() => props.handleSelectAnswer(props.questionIndex, answer)}
+              >
+                {decodeQuestionString(answer)}
+              </p>
+              {getAnswerClassName(answer)===`${styles.answer} ${styles.answer_selected} ${styles.answer_correct}` && <i className="fa-solid fa-check"></i>}
+            </div>
+          ))
+        }
+      </div>
+      <hr className={styles.question_separator} />
+    </div>
+  )
+}
